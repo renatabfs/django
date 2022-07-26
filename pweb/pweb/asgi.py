df34e40924741a659e9b85f08d90e7b4ed81e1ef
Aluno <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>
2022-07-26 11:19:31 -0300

    Atualização do django

--- pweb/pweb/asgi.py
+++ pweb/pweb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pwb project.
+ASGI config for pweb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pwb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pweb.settings')
 
 application = get_asgi_application()
