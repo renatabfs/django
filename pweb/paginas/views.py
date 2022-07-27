7cd853f26ac407fb25dafbbcf8755b9e49e5e790
renatabfs <renata.niall66@gmail.com>
2022-07-27 08:53:06 -0300

    Adicionei as urls

--- pweb/paginas/views.py
+++ pweb/paginas/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .views import IndexView
 
-# Create your views here.
+urlpatterns = [
+    path ('inicio/', IndexView.as_view (), name = 'inicio')
+]
