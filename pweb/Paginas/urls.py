d1a71820aedae69b238ba87a6b059413fb977261
Aluno <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>
2022-07-28 08:37:46 -0300

    Urls adicionadas e pasta paginas criada

---
+++ pweb/Paginas/urls.py
@@ -0,0 +1,9 @@
+from importlib.resources import path
+from unicodedata import name
+from django.urls import Path
+from .views import IndexView
+
+
+urlpatterns = [
+    path('inicio/', IndexView.as_view(), name='inicio')
+]
--- pweb/pweb/settings.py
+++ pweb/pweb/settings.py
@@ -31,12 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Pweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
